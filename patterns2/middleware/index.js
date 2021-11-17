8814dfb6a96c9f7ace9c44f64b95e5a16e2a6c4f
Oscar <oscar.andres.vargas1@gmail.com>
2021-11-17 17:24:50 +0100

    Middleware

--- patterns2/middleware/index.js
+++ patterns2/middleware/index.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const path = require("path");
+const pipeline = require("./middleware.js");
 
 const obj = {
   sum: [100, 200],
   substraction: [1999, 999],
-  multiply: [11, 11],
+  multiply: [11, 12],
   divide: [9, 3],
 };
 
@@ -22,30 +23,6 @@ fs.readFile(readdir, (error, data) => {
   if (error) {
     throw new Error(error);
   }
-  const operationsToObject = JSON.parse(data);
-  objectOperationsIterator(operationsToObject);
+  let operationsToObject = JSON.parse(data);
+  pipeline.execute(operationsToObject);
 });
-
-const objectOperationsIterator = (obj) => {
-  const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    switch (key) {
-      case "sum":
-        const sum = obj[key][0] + obj[key][1];
-        console.log(`${key}: ${obj[key]} equals to: ${sum}`);
-        break;
-      case "substraction":
-        const substraction = obj[key][0] - obj[key][1];
-        console.log(`${key}: ${obj[key]} equals to: ${substraction}`);
-        break;
-      case "multiply":
-        const multiply = obj[key][0] * obj[key][1];
-        console.log(`${key}: ${obj[key]} equals to: ${multiply}`);
-        break;
-      case "divide":
-        const divide = obj[key][0] / obj[key][1];
-        console.log(`${key}: ${obj[key]} equals to: ${divide}`);
-        break;
-    }
-  });
-};
--- patterns2/middleware/middleware.js
+++ patterns2/middleware/middleware.js
@@ -1,31 +1,106 @@
 function Pipeline(...middlewares) {
-    const stack = middlewares
-  
-    const push = (...middlewares) => {
-      stack.push(...middlewares)
-    }
-  
-    const execute = async (context) => {
-      let prevIndex = -1
-  
-      const runner = async (index) => {
-        if (index === prevIndex) {
-          throw new Error('next() called multiple times')
-        }
-  
-        prevIndex = index
-  
-        const middleware = stack[index]
-  
-        if (middleware) {
-          await middleware(context, () => {
-            return runner(index + 1)
-          })
-        }
+  const stack = middlewares;
+
+  const push = (...middlewares) => {
+    stack.push(...middlewares);
+  };
+
+  const execute = async (context) => {
+    let prevIndex = -1;
+
+    const runner = async (index) => {
+      if (index === prevIndex) {
+        throw new Error("next() called multiple times");
       }
-  
-      await runner(0)
-    }
-  
-    return { push, execute }
+
+      prevIndex = index;
+
+      const middleware = stack[index];
+
+      if (middleware) {
+        await middleware(context, () => {
+          return runner(index + 1);
+        });
+      }
+    };
+
+    await runner(0);
+  };
+
+  return { push, execute };
+}
+
+//  create a middleware pipeline
+const pipeline = Pipeline(
+  // with an initial middleware - number^2
+  (obj, next) => {
+    console.log("Initial numbers:", obj);
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+      obj[key][0] = Math.pow(obj[key][0], 2);
+    });
+    keys.forEach((key) => {
+      obj[key][1] = Math.pow(obj[key][1], 2);
+    });
+
+    console.log("Squaring the numbers:", obj);
+    next();
   }
+);
+
+pipeline.push(
+  // middleware - number^3
+  (obj, next) => {
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+      obj[key][0] = Math.pow(obj[key][0], 3);
+    });
+    keys.forEach((key) => {
+      obj[key][1] = Math.pow(obj[key][1], 3);
+    });
+
+    console.log("Cubing the numbers:", obj);
+    next();
+  },
+
+  // middleware - dividing items
+  (obj, next) => {
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+      const firstDivision = obj[key][0] / obj[key][1];
+      const secondDivision = obj[key][1] / obj[key][0];
+      obj[key][0] = firstDivision;
+      obj[key][1] = secondDivision;
+    });
+
+    console.log("Dividing the numbers:", obj);
+    next();
+  },
+
+  // last middleware - sum, substraction, multiply, divide
+  (obj, next) => {
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+      switch (key) {
+        case "sum":
+          const sum = obj[key][0] + obj[key][1];
+          console.log(`${key}: ${obj[key]} equals to: ${sum}`);
+          break;
+        case "substraction":
+          const substraction = obj[key][0] - obj[key][1];
+          console.log(`${key}: ${obj[key]} equals to: ${substraction}`);
+          break;
+        case "multiply":
+          const multiply = obj[key][0] * obj[key][1];
+          console.log(`${key}: ${obj[key]} equals to: ${multiply}`);
+          break;
+        case "divide":
+          const divide = obj[key][0] / obj[key][1];
+          console.log(`${key}: ${obj[key]} equals to: ${divide}`);
+          break;
+      }
+    });
+  }
+);
+
+module.exports = pipeline;
