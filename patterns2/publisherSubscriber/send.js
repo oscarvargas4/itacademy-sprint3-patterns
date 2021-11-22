45db41a1bb8760a8a3c962ea080fd6cd95f16f6c
Oscar Vargas <85995422+oscarvargas4@users.noreply.github.com>
2021-11-22 12:08:53 +0100

    Update

--- patterns2/publisherSubscriber/send.js
+++ patterns2/publisherSubscriber/send.js
@@ -1,16 +1,15 @@
 var amqp = require("amqplib/callback_api");
 
-// connect to RabbitMQ server
-amqp.connect("amqp://localhost", function (error0, connection) {});
-
 class Publisher {
   sendMessage(queueName, messageText) {
-    // we create a channel, which is where most of the API for getting things done resides
+    
+    // connect to RabbitMQ server
     amqp.connect("amqp://localhost", function (error0, connection) {
       if (error0) {
         throw error0;
       }
 
+      // we create a channel, which is where most of the API for getting things done resides
       // To send, we must declare a queue for us to send to; then we can publish a message to the queue:
       connection.createChannel(function (error1, channel) {
         if (error1) {
